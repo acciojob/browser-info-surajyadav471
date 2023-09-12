7a431cbc2ffab0818f7c1bc65daa3c546848616f
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-09-12 09:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+window.onload = function() {
+      const browserInfo = getBrowserInfo();
+      displayMessage(browserInfo);
+    };
+
+    function getBrowserInfo() {
+      const browserName = navigator.appName;
+      const browserVersion = navigator.appVersion;
+      return {
+        name: browserName,
+        version: browserVersion
+      };
+    }
+
+    function displayMessage(browserInfo) {
+		
+      const message = "You are using " + browserInfo.name + " " + browserInfo.version;
+      document.getElementById("browser-info").innerHTML = message;
+    }
